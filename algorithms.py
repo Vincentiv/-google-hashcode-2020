85b9b6d6d701113e73a99d42188f4a587390921d
Anais <akayaturan@gmail.com>
2020-02-16 17:19:58 +0100

    dynamic programming algo

--- algorithms.py
+++ algorithms.py
@@ -1,2 +1,22 @@
-def do_sth(data):
-    pass
+def dynamic_prog(w, nb_elements, w_array):
+    matrix = [[0 for x in range(w)] for y in range(nb_elements)]
+
+    for j in range(w):
+        matrix[0][j] = 0
+    print(matrix)
+
+    for i in range(nb_elements):
+        for j in range(w):
+            if w_array[i] > j:
+                matrix[i][j] = matrix[i-1][j]
+            else:
+                matrix[i][j] = max(matrix[i-1][j], matrix[i-1][j-w_array[i]] + w_array[i])
+
+    return matrix[nb_elements-1][w-1]
+
+
+if __name__ == "__main__":
+    w = 100
+    i = 10
+    w_array = [4, 14, 15, 18, 29, 32, 36, 82, 95, 95]
+    print(dynamic_prog(w, i, w_array))
--- input_output.py
+++ input_output.py
@@ -1,7 +1,12 @@
+from algorithms import *
+
 def parse_in(in_file):
     with open(in_file, 'r') as f:
-        for line in f:
-            print(line)
+        lines = f.readlines()
+        max_slices = int(lines[0].rstrip('\n').split(" ")[0])
+        pizza_types_count = int(lines[0].rstrip('\n').split(" ")[1])
+        pizza_slices = list(map(int, lines[1].rstrip('\n').split(" ")))
+        return max_slices, pizza_types_count, pizza_slices
 
 
 def write_solution(out_file, line_to_write):
@@ -10,5 +15,6 @@ def write_solution(out_file, line_to_write):
 
 
 if __name__ == "__main__":
-    path = "/home/anais/Downloads/a_example.in"
-    parse_in(path)
+    path = "/home/anais/Downloads/d_quite_big.in"
+    max_slices, pizza_types_count, pizza_slices = parse_in(path)
+    print(dynamic_prog(max_slices, pizza_types_count, pizza_slices))
