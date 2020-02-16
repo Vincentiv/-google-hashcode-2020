4cd8578f25bd9e48f0969bb2597d2455ae4381c8
Justin <justin.galerne@hotmail.fr>
2020-02-16 17:01:33 +0100

    Fix brute force

--- algorithms.py
+++ algorithms.py
@@ -5,10 +5,10 @@ def do_sth(max_slices, pizza_types_count, pizza_slices):
 
 
 def brute_force(m, n, slices):
-    return types_reccurs(m, 0, n, slices, [])
+    return types_reccurs(m, 0, n, slices, [], 0)
 
 
-def types_reccurs(m, m_temp, n, slices, slices_temp):
+def types_reccurs(m, m_temp, n, slices, slices_temp, i):
     if len(slices) == 0:
         return m_temp, slices_temp
     if m_temp >= m:
@@ -17,8 +17,8 @@ def types_reccurs(m, m_temp, n, slices, slices_temp):
         return m_temp, slices_temp
     else:
         s0 = slices[0]
-        m_temp0, slices_temp0 = types_reccurs(m, m_temp + s0, n - 1, slices[1:], slices_temp + [s0])
-        m_temp1, slices_temp1 = types_reccurs(m, m_temp, n, slices[1:], slices_temp)
+        m_temp0, slices_temp0 = types_reccurs(m, m_temp + s0, n - 1, slices[1:], slices_temp + [i], i+1)
+        m_temp1, slices_temp1 = types_reccurs(m, m_temp, n, slices[1:], slices_temp, i+1)
         if m_temp0 < m_temp1:
             if m_temp1 <= m:
                 return m_temp1, slices_temp1
