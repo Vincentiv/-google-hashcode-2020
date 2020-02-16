ae11a0103204455c6f505ea21b4ab58397707279
Xavier Rettel <xavier.rettel@student.ecp.fr>
2020-02-16 15:30:59 +0100

    Input function

--- input_output.py
+++ input_output.py
@@ -1,15 +1,23 @@
+import os
+
+
 def parse_in(in_file):
     with open(in_file, 'r') as f:
-        for line in f:
-            print(line)
+        lines = f.readlines()
+        max_slices = lines[0].rstrip('\n').split(" ")[0]
+        pizza_types_count = lines[0].rstrip('\n').split(" ")[1]
+        pizza_slices = lines[1].rstrip('\n').split(" ")
+        return max_slices, pizza_types_count, pizza_slices
 
 
-def write_solution(out_file,pizza_ids=[]):
+def write_solution(out_file, pizza_ids=[]):
     with open(out_file, 'w') as f:
         f.write(len(pizza_ids))
         f.write(" ".join(pizza_ids))
 
 
 if __name__ == "__main__":
-    path = "/home/anais/Downloads/a_example.in"
-    parse_in(path)
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, 'data/a_example.in')
+    data = parse_in(path)
+    print(data)
