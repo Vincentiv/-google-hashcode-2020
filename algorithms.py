834da8aa68ece41e459cf8fb9434466ee7f867d1
Xavier Rettel <xavier.rettel@student.ecp.fr>
2020-02-20 20:20:32 +0100

    latest algo

--- algorithms.py
+++ algorithms.py
@@ -1,8 +1,11 @@
 def get_order_value(library):
-    return int(library[3])
+    return library[5]/int(library[2])*int(library[3])
+
 
 def do_sth(books_count, libraries_count, days_count, book_scores, libraries):
-    print(libraries)
+    for library in libraries:
+        library += [sum([int(book_scores[int(book)]) for book in library[4]])]
+        library[4] = sorted(library[4], key=lambda x: book_scores[int(x)], reverse=True)
     libraries_ordered = reversed(sorted(libraries, key=get_order_value))
     # la liste des libraries sous la forme [[library_id1, [book_id0, book_id1, book_id2]], [library_id2, [book_id3, book_id4]]
     solution = [[library[0], library[4]] for library in libraries_ordered]
