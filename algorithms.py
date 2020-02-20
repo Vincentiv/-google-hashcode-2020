a9c6f55036dd54c7976b6a6a3f99951c41324017
Xavier Rettel <xavier.rettel@student.ecp.fr>
2020-02-20 20:00:29 +0100

    algorithm

--- algorithms.py
+++ algorithms.py
@@ -1,9 +1,9 @@
-def get_signup_time(library):
-    return library[2]
+def get_order_value(library):
+    return int(library[3])
 
 def do_sth(books_count, libraries_count, days_count, book_scores, libraries):
     print(libraries)
-    libraries_ordered = sorted(libraries, key=get_signup_time)
+    libraries_ordered = reversed(sorted(libraries, key=get_order_value))
     # la liste des libraries sous la forme [[library_id1, [book_id0, book_id1, book_id2]], [library_id2, [book_id3, book_id4]]
     solution = [[library[0], library[4]] for library in libraries_ordered]
     return solution
