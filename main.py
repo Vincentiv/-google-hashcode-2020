a1b9ef98f619f77e8cd76cec2c2a4e22ee4fc4e1
Vincent <ipvincentiv@gmail.com>
2020-02-20 20:04:23 +0100

    cuz mac sucks

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     data_path = os.path.join(dir_name, 'data')
     input_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f))]
     for input_file in input_files:
+        if (input_file)== ".DS_Store":
+            continue
         input_path = os.path.join(dir_name, f'data/{input_file}')
         books_count, libraries_count, days_count, book_scores, libraries = parse_in(input_path)
         solution = do_sth(books_count, libraries_count, days_count, book_scores, libraries)
